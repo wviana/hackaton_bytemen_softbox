dcb9f4498488d4a4a0333747cb25c35714ebf35b
Maiky Alves da Silva <maikymk@hotmail.com>
2017-02-05 03:13:52 -0200

    Correcao driver postgres

--- helper/postgres.js
+++ helper/postgres.js
@@ -1,66 +1,81 @@
 'use strict'
 
-const pg = require('pg')  
-const conString = 'postgres://root:root@192.168.99.101/teste' // make sure to match your own database's credentials
+var _ = require('lodash')
 
-pg.connect(conString, function (err, client, done) {  
-  if (err) {
-    return console.error('error fetching client from pool', err)
-  }
+class PostgresDriver {
+	constructor(host, user, password, database) {
+		this.pg 	   = require('pg')
 
-  var sql = 
-	`WITH keys AS (
-	    SELECT    
-	    tc.table_name,
-	    tc.constraint_name,
-	    tc.constraint_type,
-	    ccu.table_name AS fk_table,
-	    ccu.column_name AS fk_column
-	    FROM information_schema.table_constraints tc
-	    LEFT JOIN information_schema.constraint_column_usage  ccu USING(constraint_name)
-	    WHERE 1 = 1
-	        AND tc.constraint_schema = 'public'
-	        AND tc.constraint_type IN  ('FOREIGN KEY', 'PRIMARY KEY')
-	)
-	SELECT
-	    c.table_name,
-	    c.column_name,
-	    c.data_type,
-	    c.character_maximum_length,
-	    c.is_nullable,
-	    CASE
-	        WHEN fks.constraint_type = 'PRIMARY KEY' THEN TRUE 
-	        ELSE FALSE 
-	    END AS pk,
-	    CASE
-	        WHEN fks.constraint_type = 'FOREIGN KEY' THEN TRUE 
-	        ELSE FALSE 
-	    END AS fk,
-	    CASE c.table_name
-	        WHEN fks.fk_table THEN NULL
-	        ELSE fks.fk_table
-	    END AS fk_table,
-	    CASE c.table_name
-	        WHEN fks.fk_table THEN NULL
-	        ELSE fks.fk_column
-	    END AS fk_column
-	FROM  information_schema.columns AS c
-	LEFT JOIN keys fks ON 1 = 1
-	    AND fks.table_name = c.table_name
-	    AND fks.fk_column = c.column_name
-	WHERE 1 = 1
-	    AND c.table_schema = 'public';
-	`;
+		this.conString = `postgres://${user}:${password}@${host}/${database}` // make sure to match your own database's credentials
+	}
 
-	//console.log(sql);
+	getSchema(onSuccess) {
+		this.pg.connect(this.conString, function (err, client, done) {
+			if (err) {
+				return console.error('error fetching client from pool', err)
+			}
 
-  client.query(sql, [], function (err, result) {
-    done()
+			var sql = 
+				`WITH keys AS (
+				    SELECT    
+				    tc.table_name,
+				    tc.constraint_name,
+				    tc.constraint_type,
+				    ccu.table_name AS fk_table,
+				    ccu.column_name AS fk_column
+				    FROM information_schema.table_constraints tc
+				    LEFT JOIN information_schema.constraint_column_usage  ccu USING(constraint_name)
+				    WHERE 1 = 1
+				        AND tc.constraint_schema = 'public'
+				        AND tc.constraint_type IN  ('FOREIGN KEY', 'PRIMARY KEY')
+				)
+				SELECT
+				    c.table_name,
+				    c.column_name,
+				    c.data_type,
+				    c.character_maximum_length,
+				    c.is_nullable,
+				    CASE
+				        WHEN fks.constraint_type = 'PRIMARY KEY' THEN TRUE 
+				        ELSE FALSE 
+				    END AS pk,
+				    CASE
+				        WHEN fks.constraint_type = 'FOREIGN KEY' THEN TRUE 
+				        ELSE FALSE 
+				    END AS fk,
+				    CASE c.table_name
+				        WHEN fks.fk_table THEN NULL
+				        ELSE fks.fk_table
+				    END AS fk_table,
+				    CASE c.table_name
+				        WHEN fks.fk_table THEN NULL
+				        ELSE fks.fk_column
+				    END AS fk_column
+				FROM  information_schema.columns AS c
+				LEFT JOIN keys fks ON 1 = 1
+				    AND fks.table_name = c.table_name
+				    AND fks.fk_column = c.column_name
+				WHERE 1 = 1
+				    AND c.table_schema = 'public';
+			`;
 
-    if (err) {
-      return console.error('error happened during query', err)
-    }
-    console.log(result.rows)
-    process.exit(0)
-  })
-})
+			client.query(sql, [], function (err, result) {
+				done()
+
+				if (err) {
+				  	return console.error('error happened during query', err)
+				}
+
+				var schema = _.groupBy(result.rows, 'table_name')
+
+				onSuccess(schema)
+
+				process.exit(0)
+			})
+		})
+	}
+}
+
+var pg = new PostgresDriver('192.168.99.101', 'root', 'root', 'teste')
+
+pg.getSchema((res) => console.log(res))
