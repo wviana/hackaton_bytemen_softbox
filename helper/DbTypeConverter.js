af9578a693dc2dd00099972d9ec63559e528304d
Wesley Viana <viana.wesley@gmail.com>
2017-02-05 06:02:36 -0200

    Criado helper para converter tipo do pg para java

---
+++ helper/DbTypeConverter.js
@@ -0,0 +1,32 @@
+module.exports = function() {
+	var toJavaTypes = {
+		"bigint": "long",
+		"bigserial": "long",
+		"boolean": "boolean",
+		"character": "String",
+		"character varying": "String",
+		"date": "Date",
+		"double precision": "double",
+		"integer": "int",
+		"money": "double",
+		"numeric": "double",
+		"real": "double",
+		"smallint": "int",
+		"smallserial": "int",
+		"serial": "long",
+		"text": "String",
+		"time": "Date",
+		"time": "Date",
+		"timestamp": "Date",
+		"timestamp": "Date",
+		"uuid": "long"
+	};
+	
+	return function(dbType) {
+		return toJavaTypes[dbType];
+	};
+}();
+
+
+
+
